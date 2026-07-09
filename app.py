286f2ae61e8ee49866ad4e38ea1008da8262e89c
Arda Mert Dedeoğlu <mert.dedeoglu@std.yildiz.edu.tr>
2026-07-09 21:23:21 +0300

    added base login functionality

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+import os
+
 from flask import Flask, request
-from services.auth import register
+from dotenv import load_dotenv
+
+from services.auth import register, login
 app = Flask(__name__)
 
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
 
 @app.post("/api/auth/register")
 def handle_register():
     data = request.get_json() or {}
     return register(data)
+
+@app.post("/api/auth/login")
+def handle_login():
+    data = request.get_json() or {}
+    return login(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- services/auth.py
+++ services/auth.py
@@ -1,12 +1,17 @@
+import os
 from typing import Dict
 import sqlite3
-from flask import jsonify
+from flask import jsonify, make_response, session, redirect, url_for
 import bcrypt
 
 from datetime import datetime
 
 from db import make_connection, close_connection
+import secrets
 
+import time
+
+from itsdangerous import URLSafeSerializer
 
 def register(data: Dict[str, str]):
     email = data["email"]
@@ -42,4 +47,38 @@ def register(data: Dict[str, str]):
 
     close_connection(conn)
     return jsonify({"success" : "true", "email" : email, "created_at": date_string}), 201
+
+def login(data: Dict[str, str]):
+    email = data["email"]
+    password = data["password"]
+    if email is None or password is None:
+        return jsonify({"error": "email and/or password missing"}), 400
+
+    conn = make_connection()
+    if conn is None:
+        return jsonify({"error": "database connection failed"}), 500
+
+    cursor = conn.cursor()
+    encoded_password = password.encode("utf-8")
+    cursor.execute("""
+    SELECT id, password_hash FROM users
+    WHERE email = ?
+    """, (email,))
+
+    row = cursor.fetchone()
+    if row is None:
+        return jsonify({"error": "an account with given email is not found"}), 404
+
+    if bcrypt.checkpw(encoded_password, row[1]):
+        user_id = row[0]
+        session["user_id"] = user_id
+        return jsonify({"success": "true", "session": session["user_id"]}), 200
+    else:
+        return jsonify({"error": "invalid email or password"}), 401
+
+
+
+
+
+
 
